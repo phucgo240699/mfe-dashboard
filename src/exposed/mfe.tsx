60e4a010aa0b44f73403d4ee166acc00864546d4
phucgo240699 <phucgo240699@gmail.com>
2025-01-28 08:34:24 +0700

    rename pages components

--- src/exposed/mfe.tsx
+++ src/exposed/mfe.tsx
@@ -1,21 +1,21 @@
+import { SuspenseLayer } from '@/components/SuspenseLayer';
 import './index.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
-import { SuspenseLayer } from '@/components/SuspenseLayer';
 
-const Dashboard = React.lazy(() => import('@/pages/dashboard'));
-const Table = React.lazy(() => import('@/pages/table'));
+const DashboardPage = React.lazy(() => import('@/pages/dashboard'));
+const TablePage = React.lazy(() => import('@/pages/table'));
 
 const App = () => {
   return (
     <BrowserRouter basename="/dashboard">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<DashboardPage />} />
         <Route
           path="/table"
           element={
             <SuspenseLayer>
-              <Table />
+              <TablePage />
             </SuspenseLayer>
           }
         />
